df36e15b96bcc3a725406e19535e7e8dd418e3d1
mgh <m_ghorbanian@aut.ac.ir>
2021-01-27 23:02:42 +0330

    testdfsdfgsdf

--- finalgit/src/com/company/Main.java
+++ finalgit/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
+        System.out.println("hello world");
 	// write your code here
     }
 }
