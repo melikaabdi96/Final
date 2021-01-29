0f286e8e9422892b1a955fa7edf8cc316412cc5d
morvarid-L-noor <morvarid7777@gmail.com>
2021-01-28 23:31:37 -0800

    plant added

---
+++ finalgit/src/com/company/Plant.java
@@ -0,0 +1,15 @@
+package com.company;
+public class Plant extends Element{
+    protected int cost ;
+
+
+    public Plant(){
+
+    }
+
+
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+
+}
