8b7ce2554a3e341c334bea3cbb9e44be0ce4144e
melikaabdi96 <melikaabdi_pc@yahoo.com>
2021-01-29 20:37:30 +0330

    sun with javadoc

--- finalgit/src/com/company/Sun.java
+++ finalgit/src/com/company/Sun.java
@@ -3,10 +3,22 @@ package com.company;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+/**
+ * This class holds informations of sun
+ *
+ * @author Melika & Morvarid
+ * @since 2021
+ */
 public class Sun extends Element{
     private final int value = 25;
 //    private final int lastingTime = 10;
 
+    /**
+     *
+     * @param x location of sun
+     * @param y location of sun
+     * @throws IOException
+     */
     public Sun(int x, int y) throws IOException {
         setX(x);
         setY(y);
@@ -14,6 +26,7 @@ public class Sun extends Element{
         startTime = System.currentTimeMillis();
     }
 
+    //getter
     public int getValue() {
         return value;
     }
