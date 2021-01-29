d73722158ce1c2f80849aed99d84aa5ef62210ed
melikaabdi96 <melikaabdi_pc@yahoo.com>
2021-01-29 20:39:50 +0330

    sound with javadoc

--- finalgit/src/com/company/Sound.java
+++ finalgit/src/com/company/Sound.java
@@ -3,11 +3,24 @@ package com.company;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+/**
+ * This class holds informations of sound
+ *
+ * @author Melika & Morvarid
+ * @since 2021
+ */
 public class Sound {
     private static String soundsPath = "sounds";
     private String soundName;
     AudioInputStream audioInputStream;
     Clip clip;
+
+    /**
+     * @param soundName name of sound
+     * @throws LineUnavailableException
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     */
     public Sound(String soundName) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
         this.soundName = soundName;
         audioInputStream = AudioSystem.getAudioInputStream(new File(soundsPath+"\\"+soundName).getAbsoluteFile());
@@ -16,9 +29,12 @@ public class Sound {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
 
     }
+
+    //playe sound
     public void play(){
         clip.start();
     }
+
     // Method to reset audio stream
     public void resetAudioStream() throws UnsupportedAudioFileException, IOException,
             LineUnavailableException
