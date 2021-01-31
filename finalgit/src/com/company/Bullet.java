b3e5b1b6220a547fcf00c0b5302d9d5f6dfeafb9
melikaabdi96 <melikaabdi_pc@yahoo.com>
2021-01-31 14:20:53 +0330

    bullet with javadoc

--- finalgit/src/com/company/Bullet.java
+++ finalgit/src/com/company/Bullet.java
@@ -1,51 +1,81 @@
 package com.company;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+/**
+ * This class holds informations of bullets
+ *
+ * @author Melika & Morvarid
+ * @since 2021
+ */
 public class Bullet extends Element {
     private int damage ;
+    private boolean collided ;
+    private BufferedImage bImage;
     public enum bulletType{normal , frozen}
-    private bulletType bulletType;
+    private static  bulletType bulletType;
     public Bullet(int x , int y , bulletType bulletType) throws Exception {
         setX(x);
         setY(y);
-        setRow((y-130)/120);
         this.bulletType = bulletType;
         setFields();
+        collided = false;
     }
 
+    //getter
+    public boolean isCollided() {
+        return collided;
+    }
+
+    //setter
+    public void setCollided(boolean collided) {
+        this.collided = collided;
+    }
+
+    //getttr
     public Bullet.bulletType getType() {
         return bulletType;
     }
 
+    //setter
     public void setType(Bullet.bulletType type) {
         this.bulletType = type;
     }
+
+    @Override
     protected void setFields() throws Exception {
         if(bulletType == bulletType.normal){
-
             this.damage = 30;
-            img = ImageIO.read(new File("images\\pea.png"));
-
+            bImage = ImageIO.read(new File("images\\pea.png"));
         }else if(bulletType == bulletType.frozen){
-
             this.damage = 35;
-            img = ImageIO.read(new File("images\\freezepea.png"));
-
-
+            bImage = ImageIO.read(new File("images\\freezepea.png"));
         }else throw new Exception(" No bullet type found !!!!");
     }
 
+    //getter
     public int getDamage() {
         return damage;
     }
 
 
-
+    /**
+     * @return true if x of bullet is more than 1000
+     */
     public boolean endOfFeild(){
         if(this.getX() > 1000)return true;
         else return false;
     }
 
-}
+    //getter
+    public Image getbImage() {
+        return bImage;
+    }
 
+    //setter
+    public void setbImage(BufferedImage bImage) {
+        this.bImage = bImage;
+    }
+}
