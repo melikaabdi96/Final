e13f50c8ea3dff25de8ff69785b8b0cdda66dc30
melikaabdi96 <melikaabdi_pc@yahoo.com>
2021-01-31 14:46:17 +0330

    plant updated

--- finalgit/src/com/company/Plant.java
+++ finalgit/src/com/company/Plant.java
@@ -1,14 +1,18 @@
 package com.company;
 
+/**
+ * This class holds informations of plants
+ *
+ * @author Melika & Morvarid
+ * @since 2021
+ */
 public class Plant extends Element{
+    //cost of plant
     protected int cost ;
 
+    public Plant(){}
 
-    public Plant(){
-
-    }
-
-
+    //setter
     public void setCost(int cost){
         this.cost = cost;
     }
